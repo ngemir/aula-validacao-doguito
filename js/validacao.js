20b14516b71e214f0820b100f628bcfd846f335a
Emir Takayama <ngemir@gmail.com>
2022-09-15 17:05:05 -0300

    Refeito para funcionar em input geral.

--- js/validacao.js
+++ js/validacao.js
@@ -1,23 +1,29 @@
-const dataNascimento = document.querySelector('#nascimento');
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-dataNascimento.addEventListener('blur', (evento) => {
-    validaDataNascimento(evento.target);
-})
-
-function validaDataNascimento(input) {
-    const dataRecebida = new Date(input.value);
-    let mensagem = '';
-
-    if(!maiorQue18(dataRecebida)) {
-        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.';
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
     }
-
-    input.setCustomValidity(mensagem);
 }
 
-function maiorQue18(data){
-    const dataAtual = new Date();
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
 
-    return dataMais18 <= dataAtual;
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
 }
